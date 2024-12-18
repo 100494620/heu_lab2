65725d6c887fe9f511d7efe46084d23353e58162
100494620 <100494620@alumnos.uc3m.es>
2024-12-18 13:48:04 +0100

    franj and taller capacities restr added

--- CSPMaintenance.py
+++ CSPMaintenance.py
@@ -1,6 +1,7 @@
 from constraint import *
 import os
 import sys
+from collections import Counter
 
 class Plane:
     def __init__(self, id, tipo, restr, t1, t2):
@@ -9,6 +10,15 @@ class Plane:
         self.restr = restr
         self.t1 = t1
         self.t2 = t2
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
 
 def read_input_file(file_path):
     with open(file_path, 'r') as file:
@@ -49,39 +59,55 @@ def parse_data(data):
     planes = []
     for plane_info in data[5:]:
         info = plane_info.strip().split("-")
-
         planes.append(Plane(info[0], info[1], info[2], info[3], info[4]))
     #print(prk_positions)
     return franjas, matrix_size, std_positions, spc_positions, prk_positions, planes
 
-#def restrictions(franjas, matrix_size, std_positions, spc_positions, prk_positions, planes):
 
-def jmb_adjacent(*args):
-    positions = []
-    plane_types = []
+def franja_capacity(*args):
+    # problem.addConstraint(jmb_adjacent, variables_to_time[i])
+    # 0: (plane_0,0), (plane_1,0), (plane_2,0)
+    #   (pos_plane0_0), (pos_plane1_0), (pos_plane2_0)
+    # each taller can assist no more than 2 planes in one time slot
+    if any(count > 2 for count in Counter(args).values()):
+        return False
+    return True
 
-    """for plane in planes:
-        plane_types.append(plane.tipo)
-
-    for i in range(len(planes) - 1):
-        for j in range(i + 1, len(positions)):
-            if plane_types[i] == plane_types[j] == "JMB":
-                if abs(positions[i][0] - positions[j][0]) + abs(positions[i][1] - positions[j][1]) == 1:
-                    return False
-
-    return True"""
+def taller_capacity(*args):
+    # there is no JUMBO planes in the same time slot at the same taller
+    if any(count > 1 for count in Counter(args).values()):
+        return False
+    return True
 
 def main():
     problem = Problem()
     franjas, matrix_size, std_positions, spc_positions, prk_positions, planes = parse_data(read_data())
 
-    variables = []
+    variables = [] # [(plane: Plane, time_slot: int)]
+    # problem.constraints: [((plane: Plane, time_slot: int), [pos, pos])]
 
     positions = std_positions + spc_positions + prk_positions
 
+    # set values
     for plane in planes:
         for franja in range(0, franjas):
             variables.append((plane, franja))
+
+    variables_to_time = {}
+    jumbo_to_time = {}
+    #0: (plane_0,0), (plane_1,0), (plane_2,0)
+    #1: (plane_0,1), (plane_1,1), (plane_2,1)
+    # ....
+
+    for var in variables:
+        if var[1] not in variables_to_time:
+            variables_to_time[var[1]] = []
+        variables_to_time[var[1]].append(var)
+
+        if var[0].tipo == "JMB" not in variables_to_time:
+            jumbo_to_time[var[1]] = []
+        jumbo_to_time[var[1]].append(var)
+
     # assures that all planes will be assigned to the unique taller/parking in each time slot
     for var in variables:
         problem.addVariable(var, positions)
@@ -89,68 +115,14 @@ def main():
     # variable - (plane, time-slot)
     # map <int time slot, planes>
     for i in range(0, franjas):
-        problem.addConstraint(jmb_adjacent, (planes, i))
+        problem.addConstraint(franja_capacity, variables_to_time[i])
+        problem.addConstraint(taller_capacity, jumbo_to_time[i])
 
     #print(variables)
     #print(domain)
     solutions = problem.getSolutions()
     print(solutions)
-    #print(len(solutions))
+    print(len(solutions))
 
 if __name__ == "__main__":
     main()
-
-# A continuacion se anyaden las restricciones del problema mediante la funcion addConstraint proporcionada por la libreria
-#
-# Ejemplos:
-# problem.addConstraint(lambda a, b: a > b, ('a', 'b'))		Crea una funcion lambda que recibe dos parametros que se corresponden
-# con los valores de las variables 'a' y 'b', y comprueba que 'a' es mayor que 'b'. Tambien se podria haber creado una funcion para
-# comprobar este hecho:
-#
-# def greater(a, b):
-#    if a > b:
-#	return True
-#
-# problem.addConstraint(greater, ('a', 'b'))
-#
-# En este caso vamos a modelar en primer lugar la restriccion de que Alfredo y Ruben no quieren trabajar juntos, es decir,
-# RA,R = [(1,2),(1,3),(2,1),(2,3),(3,1),(3,2)]. Se puede hacer de varias formas. Una de ellas es definir una funcion, por ejemplo,
-# notEqual, que compruebe que el valor de una variable es diferente de la de la otra:
-
-"""def notEqual(a, b):
-	if a != b:
-		return True
-
-problem.addConstraint(notEqual, ('A', 'R'))
-
-# Tambien se puede crear una funcion lambda para hacer esta comprobacion:
-
-problem.addConstraint(lambda a, b: a != b, ('A', 'R'))
-
-
-# Por ultimo, la libreria ofrece la funcion AllDifferentConstraint que precisamente comprueba que el valor de una variable es diferente a las de las otras:
-
-problem.addConstraint(AllDifferentConstraint(), ['A', 'R'])
-
-# Lo anterior, son tres formas diferentes de modelar la misma restriccion RA,R = [(1,2),(1,3),(2,1),(2,3),(3,1),(3,2)]
-# Ahora modelamos la restriccion de que Ruben y Felisa quieren trabajar en la misma parte RR,F={(1,1),(2,2),(3,3)}
-
-problem.addConstraint(lambda a, b: a == b, ('R', 'F'))
-
-
-# Por ultimo, modelamos la restriccion de que Yara hace una parte posterior a la que haga Ruben, RR,Y={(1,2),(1,3),(2,3)}
-
-def consecutive(a, b):
-	if b > a:
-		return True
-
-problem.addConstraint(consecutive, ('R', 'Y'))
-
-# Una vez modelado el problema, podemos recuperar una de las soluciones:
-
-print(problem.getSolution())
-
-# o todas las soluciones:
-
-print(problem.getSolutions())
-print(len(problem.getSolutions()))"""
